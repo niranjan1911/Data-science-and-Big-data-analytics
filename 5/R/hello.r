16806507792720cf263c9d1d53ce1bb58cb5a181
Niranjan Patil <52923629+niranjan1911@users.noreply.github.com>
2020-03-11 14:53:34 +0530

    Add files via upload

---
+++ 5/R/hello.r
@@ -0,0 +1,3 @@
+print("Hello World!")
+a <- rnorm(100)
+plot(a)
--- 5/graphinfile.py
+++ 5/graphinfile.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("/home/hduser/33356/5/dataset.csv")
 
-plt.bar(df["name"],df["mark"])
+plt.bar(df["roll"],df["mark"])
 plt.show()
 
 
